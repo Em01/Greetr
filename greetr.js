09cb119aa33ddb01dfecd5fddc728e2dd4767f6e
Em01 <emma_williams1@hotmail.co.uk>
2015-10-31 19:04:45 +0000

    Setup wit iife

--- greetr.js
+++ greetr.js
@@ -1,5 +1,11 @@
 (function(global, $) {
 
+	var Greetr = function(firstname, lastname, language) {
+		return new Greetr.init(firstname, lastname, language);
+	}
 
+	Greetr.init = function(firstname, lastname, language) {
+		
+	}
 
 }(window, jQuery));
