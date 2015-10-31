32911193d1b7721643b6bfa3cfdfd7f2db0a994c
Em01 <emma_williams1@hotmail.co.uk>
2015-10-31 20:43:23 +0000

    set up the object and its prototype

--- greetr.js
+++ greetr.js
@@ -1,11 +1,19 @@
 (function(global, $) {
 
-	var Greetr = function(firstname, lastname, language) {
-		return new Greetr.init(firstname, lastname, language);
-	}
+	var Greetr = function(firstName, lastName, language) {
+		return new Greetr.init(firstName, lastName, language);
+	};
 
-	Greetr.init = function(firstname, lastname, language) {
+	Greetr.prototype = {};
+
+	Greetr.init = function(firstName, lastName, language) {
 		
-	}
+			var self = this;
+			self.firstName = firstName || '';
+			self.lastName = lastName || '';
+			self.language = language || 'en';
+	};
+
+	Greetr.init.prototype = Greetr.prototype;
 
 }(window, jQuery));
